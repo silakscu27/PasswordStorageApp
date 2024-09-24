b5e984f0d75f22bf49661777760181565ae5274f
Sila Kuscu <silaksc27@gmail.com>
2024-09-24 15:15:54 +0300

    Update Program.cs

--- PasswordStorageApp/PasswordStorageApp.WebApi/Program.cs
+++ PasswordStorageApp/PasswordStorageApp.WebApi/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using PasswordStorageApp.WebApi.Hubs;
+using PasswordStorageApp.WebApi.Persistence.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,19 +11,52 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ApplicationDbContext>();
+
+builder.Services.AddSignalR();
+
+// Add Cors for all origins
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
 var app = builder.Build();
+
+app.UseCors();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+
 }
+
+var cts = new CancellationTokenSource();
+
+await using var scope = app.Services.CreateAsyncScope();
+
+var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+var migrations = await context.Database.GetPendingMigrationsAsync(cts.Token);
+
+if (migrations.Any())
+    await context.Database.MigrateAsync(cts.Token);
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<AccountsHub>("/hubs/accountsHub");
 
 app.Run();
