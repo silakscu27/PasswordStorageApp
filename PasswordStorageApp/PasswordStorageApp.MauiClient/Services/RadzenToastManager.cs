05af2eddd64e75725e34a3a5378f392900412048
Sila Kuscu <silaksc27@gmail.com>
2024-09-24 12:04:52 +0300

    Create Services folder

---
+++ PasswordStorageApp/PasswordStorageApp.MauiClient/Services/RadzenToastManager.cs
@@ -0,0 +1,54 @@
+﻿using Radzen;
+
+namespace PasswordStorageApp.MauiClient.Services
+{
+    public class RadzenToastManager : IToastService
+    {
+        private readonly NotificationService _notificationService;
+
+        public RadzenToastManager(NotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        public void Success(string message)
+        {
+            _notificationService.Notify(NotificationSeverity.Success, detail: message, duration: 10000);
+        }
+
+        public void Success(string message, string title)
+        {
+            _notificationService.Notify(NotificationSeverity.Success, summary: title, message, duration: 10000);
+        }
+
+        public void Warning(string message)
+        {
+            _notificationService.Notify(NotificationSeverity.Warning, detail: message, duration: 10000);
+        }
+
+        public void Warning(string message, string title)
+        {
+            _notificationService.Notify(NotificationSeverity.Warning, summary: title, message, duration: 10000);
+        }
+
+        public void Info(string message)
+        {
+            _notificationService.Notify(NotificationSeverity.Info, detail: message, duration: 10000);
+        }
+
+        public void Info(string message, string title)
+        {
+            _notificationService.Notify(NotificationSeverity.Info, summary: title, message, duration: 10000);
+        }
+
+        public void Error(string message)
+        {
+            _notificationService.Notify(NotificationSeverity.Error, detail: message, duration: 10000);
+        }
+
+        public void Error(string message, string title)
+        {
+            _notificationService.Notify(NotificationSeverity.Error, summary: title, message, duration: 10000);
+        }
+    }
+}
